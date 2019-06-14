6103f58c09b5237b32c3a2c2c9ecf38c6855263a
ljyTlm <970955947@qq.com>
2019-06-15 00:21:58 +0800

    简述了java的线程池

---
+++ src/main/java/多线程/多线程_线程池.java
@@ -0,0 +1,15 @@
+package 多线程;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class 多线程_线程池 {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService e = Executors.newFixedThreadPool(5);
+        e.submit(new MyThread("a"));
+        e.submit(new MyThread("b"));
+        //Thread.sleep(2000);
+//      执行完上一次提交的任务 但是不接受新任务
+        e.shutdown();
+    }
+}

7b16bf0c2cdfa545114ad1a65b8ce4d81b7ea419
ljyTlm <970955947@qq.com>
2019-06-15 00:06:31 +0800

    多线程之线程组 整理完毕

---
+++ src/main/java/多线程/多线程_线程组.java
@@ -0,0 +1,28 @@
+package 多线程;
+
+public class 多线程_线程组 {
+    public static void main(String[] args) {
+//        java允许程序直接对线程组进行操作
+//        java提供了ThreadGroup 默认情况下所有线程都属于主线程组
+        System.out.println(Thread.currentThread().getName());
+        ThreadGroup group = new ThreadGroup("我是新的线程组");
+        Thread thread1 = new Thread(group ,new GroupTest(),"张三");
+        Thread thread2 = new Thread(group ,new GroupTest(),"李四");
+        Thread thread3 = new Thread(group ,new GroupTest(),"王五");
+
+        System.out.println(thread1.getThreadGroup().getName());
+        System.out.println(thread2.getThreadGroup().getName());
+        System.out.println(thread3.getThreadGroup().getName());
+
+        group.setDaemon(true);
+//      成组操作方便省事 整个组都变成了守护线程
+
+    }
+}
+class GroupTest implements Runnable{
+    public void run() {
+        for (int i = 0; i < 10000; i ++){
+            System.out.println(Thread.currentThread().getName()+"---runTime!---"+i);
+        }
+    }
+}

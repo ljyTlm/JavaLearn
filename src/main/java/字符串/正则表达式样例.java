07151490a6cac5d395b43f6c6571d5648840b4d1
ljyTlm <970955947@qq.com>
2019-06-16 19:25:14 +0800

    正则表达式整理完毕 哇感觉超好用的诶 （*——*）

--- src/main/java/字符串/正则表达式样例.java
+++ src/main/java/字符串/正则表达式样例.java
@@ -1,5 +1,9 @@
 package 字符串;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class 正则表达式样例 {
     public static void main(String[] args) {
 //      1.样例需求
@@ -33,5 +37,51 @@ public class 正则表达式样例 {
         String str_1 = "647162381";
         String result = "[64]";
         System.out.println(str_1.matches(result));
+//      字符串拆分
+        String str_2 = "asdaafa adfaasdf sdaafa fsdafaa";
+        String[] split_1 = str_2.split("[ ]");
+        System.out.println(Arrays.toString(split_1));
+        String[] split_2 = str_2.split("[a]");
+        System.out.println(Arrays.toString(split_2));
+        String[] split_3 = str_2.split("[f][a]");
+        System.out.println(Arrays.toString(split_3));
+        String[] split_4 = str_2.split("[a]{2}");
+        System.out.println(Arrays.toString(split_4));
+        String[] split_5 = str_2.split("[a-d]");
+        System.out.println(Arrays.toString(split_5));
+//      字符串替换
+        String str_3 = "wer2asdfasd34asfasdf4dfasd4";
+        String str_4 = str_3.replaceAll("\\d", "*");
+        System.out.println(str_4);
+        String str_5 = "wer2asdfasd34asfasdf4dfasd4";
+        String str_6 = str_5.replaceAll("[0-9][a-z]", "*");
+        System.out.println(str_6);
+//      正则表达式分组功能
+        String str_7 = "aabb";
+        System.out.println(str_7.matches("(.){2}(.){2}"));
+        String str_8 = "ababab";
+        System.out.println(str_8.matches("(..){3}"));
+        String str_9 = "ababab";
+        System.out.println(str_9.matches("([a][^0-9]){3}"));
+        String str_10 = "ababab";
+        System.out.println(str_10.matches("([a][^0-9])+"));
+//      即时练习！！！
+        String str_11 = "我我....我...我.要...要学....学学..学.编..编编.编.程.程.程..程";
+//      目标：我要学编程
+        String regex_1 = "(.\\.+)";
+        System.out.println(str_11.replaceAll(regex_1, ""));
+//      Pattern类的使用
+//        这个类类似于预编译正则表达式 并且具备
+//        此类的实例是不可变的，可供多个并发线程安全使用。Matcher 类的实例用于此目的则不安全。
+        Pattern pattern_1 = Pattern.compile("[a-z]");
+        Matcher matcher_1 = pattern_1.matcher("a");
+        System.out.println(matcher_1.matches());
+//      小练习：获取手机号码
+        String str_12 = "wodeshouji hao17645479673bangbuang hhh15776547718 ";
+        Pattern pattern_2 = Pattern.compile("[0-9]{11}");
+        Matcher matcher_2 = pattern_2.matcher(str_12);
+        while (matcher_2.find()){
+            System.out.println(matcher_2.group());
+        }
     }
 }

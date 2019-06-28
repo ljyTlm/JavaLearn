43a385d646c8eec5bf0e5174c3f20e1ce6b30887
ljyTlm <970955947@qq.com>
2019-06-29 00:23:00 +0800

    简单整理了jvm 需要补充！！！！

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 40; i < 40*2; i ++){
-            int v = i*i;
-            String str = v +"";
-            int k = 0;
-            for (int j = 0; j < str.length(); j ++){
-                k += (str.charAt(j)-'0');
-            }
-            System.out.print(k+" ");
+        for (int i = 40; true; i ++){
+
         }
     }
 }

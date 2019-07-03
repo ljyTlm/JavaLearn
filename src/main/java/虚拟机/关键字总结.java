4892568af3eee54aa3b9eb9891c399ef342f73aa
ljyTlm <970955947@qq.com>
2019-07-04 00:14:39 +0800

    新增 概念总结类 多线程 和 关键字 其中整理了final关键字

---
+++ src/main/java/虚拟机/关键字总结.java
@@ -0,0 +1,22 @@
+package 虚拟机;
+
+public class 关键字总结 {
+    public static void main(String[] args) {
+
+    }
+}
+
+/*
+    1.final
+        （1）final修饰类
+            表示此类不能被继承 其内部的方法隐式为final方法
+        （2）final修饰方法
+            final修饰的方法不能被重写 但是可以被重载
+        （3）final修饰变量
+            final修饰的变量必须进行初始化，而且只能初始化一次
+            可以进行直接初始化，不然只能在所有的构造方法中初始化
+            注意：这个变量不能重新赋值，但是可以修改变量
+        （4）final不能修饰接口和抽象类
+
+
+ */

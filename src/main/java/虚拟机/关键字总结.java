d6dbd6517c5b237ea7ce54d89b0e72c06c99a4cb
ljyTlm <970955947@qq.com>
2019-07-05 22:50:35 +0800

    添加了synchronized关键字 1.可重入性 2.不可中断性/原子性 缺点效率低！！！ 无法知道是否成功获取到锁

--- src/main/java/虚拟机/关键字总结.java
+++ src/main/java/虚拟机/关键字总结.java
@@ -2,9 +2,25 @@ package 虚拟机;
 
 public class 关键字总结 {
 
-    public static void main(String[] args) {
-         int v = 1;
+
+    public static void main(String[] args) throws Exception{
+        final int [] a = {1};
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                synchronized (Object.class) {
+                    a[0] = 2;
+                    String b = null;
+                    b.toString();
+                }
+            }
+        };
+        thread.start();
+        thread.join();
+        Thread.sleep(2000);
+        System.out.println(a[0]);
     }
+
 }
 
 /*
@@ -29,5 +45,11 @@ public class 关键字总结 {
         (3)此关键字可以看为一种轻量级的sync 实现了锁的 可见性
            但是没有实现锁的 ！！！ 原子性 ！！！！也就是不会引起线程阻塞
            存在安全问题 多线程的计数器功能无法实现 ！！！
+    3.synchronized
+        (1)可重入性 在调取方法时 调取的方法自动获取当前使用的锁
+        (2)不可中断性/原子性 一旦这个锁被某个线程获取 除非锁被释放 不然其他想获取这个锁的线程只能等待
+           进入等锁池
+        (3)效率低 除非同步代码块执行不然是不会释放锁的
+        (4)无法知道是否成功获取到锁
 
  */

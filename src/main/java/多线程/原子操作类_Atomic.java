d1e23ae209c157e422d44abf47128039ac7853fe
ljyTlm <970955947@qq.com>
2019-07-21 01:05:39 +0800

    添加了重入锁 ReentrantLock 原子类Atomic

---
+++ src/main/java/多线程/原子操作类_Atomic.java
@@ -0,0 +1,9 @@
+package 多线程;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class 原子操作类_Atomic {
+    public static void main(String[] args) {
+        new AtomicInteger();
+    }
+}
---
+++ src/main/java/多线程/重入锁_ReentrantLock.java
@@ -0,0 +1,9 @@
+package 多线程;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class 重入锁_ReentrantLock {
+    public static void main(String[] args) {
+        new ReentrantLock();
+    }
+}

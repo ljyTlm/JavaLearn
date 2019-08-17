0ed9aefaac49c123729aa40666ea53fae0845b08
ljyTlm <970955947@qq.com>
2019-08-17 14:28:53 +0800

    能保证输入顺序的HashMap底层维护了一个链表

---
+++ src/main/java/集合容器/实现类底层解析/LinkedHashMap详解.java
@@ -0,0 +1,10 @@
+package 集合容器.实现类底层解析;
+
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+
+public class LinkedHashMap详解 {
+    public static void main(String[] args) {
+        LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>(3, 0.75f, true);
+    }
+}
